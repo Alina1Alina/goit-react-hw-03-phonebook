f430bf2ec5de3b2aae7dba1011cd807ef49820cd
Alina1Alina <119854810+Alina1Alina@users.noreply.github.com>
2023-04-24 13:57:57 +0300

    done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,70 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
+import { Component } from 'react';
+import { Container } from './AppStyled';
+import { ContactForm } from './ContactForm/ContactForm';
+import { ContactList } from './ContactList/ContactList';
+import { Filter } from './FilterContacts';
+import { nanoid } from 'nanoid';
 
-    </div>
-  );
+export class App extends Component {
+    state = {
+    contacts: [],
+    filter: '',
+  };
+
+  componentDidMount() {
+    const contacts = JSON.parse(localStorage.getItem('contacts'));
+    if (contacts) {
+      this.setState({ contacts });
+    }
+  }
+  componentDidUpdate(prevState) {
+    if (prevState.contacts !== this.state.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+  
+formSubmitHandler = data => {
+  const newContact = { ...data, id: nanoid() };
+  this.state.contacts.find(
+    ({ name }) => name.toLowerCase() === data.name.toLowerCase()
+  )
+    ? alert(`${data.name} is already in contacts`)
+    : this.setState(prevState => ({
+        contacts: [newContact, ...prevState.contacts],
+      }));
 };
+
+  changeFilter = e => {
+    this.setState({ filter: e.currentTarget.value });
+  };
+
+  getFiltredContacts = () => {
+    const { filter, contacts } = this.state;
+    const normalizedFilter = filter.toLowerCase();
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter)
+    );
+  };
+
+  deleteContacts = contactId => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(({ id }) => id !== contactId),
+    }));
+  };
+  render() {
+    const { filter } = this.state;
+    return (
+      <Container>
+        <h1>Phonebook</h1>
+        <ContactForm onSubmit={this.formSubmitHandler}  />
+
+        <h2>Contacts</h2>
+        <Filter changeFilter={this.changeFilter} filter={filter} />
+        <ContactList
+          contacts={this.getFiltredContacts()}
+          onDeleteContact={this.deleteContacts}
+        />
+      </Container>
+    );
+  }
+}
---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,36 @@
+import { DeleteItem, Span } from './StyledListContact';
+import PropTypes from 'prop-types';
+
+export const ContactList = ({contacts,onDeleteContact  })=>  {
+   return (
+      <div>
+        <ul>
+          {contacts.map(({ id, name, number }) => (
+            <li key={id}>
+              <Span>
+                {name}: {number}
+              </Span>
+              <DeleteItem
+                type="button"
+                onClick={() => onDeleteContact(id)}
+              >
+                Delete
+              </DeleteItem>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+
+
+ContactList.propTypes = {
+  onDeleteContact: PropTypes.func.isRequired,
+  contacts: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      number: PropTypes.string.isRequired,
+      id: PropTypes.string.isRequired,
+    })
+  ),
+};
